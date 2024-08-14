a5c0cfde1c075a779428dc70db27611f5747d1ae
uspenofilm <mariia.terebus@gmail.com>
2024-08-14 14:31:43 +0300

    styling issue

---
+++ src/components/App/App.jsx
@@ -0,0 +1,23 @@
+import Profile from "../Profile/Profile";
+import FriendList from "../FriendList/FriendList";
+import TransactionHistory from "../TransactionHistory/TransactionHistory";
+import userData from "../../userData.json";
+import friends from "../../friends.json";
+import transactions from "../../transactions.json";
+import css from "../App/App.module.css";
+
+export default function App() {
+  return (
+    <div className={css.container}>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </div>
+  );
+}
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,28 @@
+import css from "../Profile/Profile.module.css";
+
+export default function Profile({ name, tag, location, image, stats }) {
+  return (
+    <div className={css.profileContainer}>
+      <div className={css.profileImgText}>
+        <img src={image} alt="User avatar" className={css.profileImg} />
+        <p className={css.profileName}>{name}</p>{" "}
+        <p className={css.profileText}>@{tag}</p>
+        <p className={css.profileText}>{location}</p>
+      </div>
+      <ul className={css.profileStats}>
+        <li className={css.profileStatsEl}>
+          <span>Followers</span>
+          <span className={css.profileStatsNumber}>{stats.followers}</span>
+        </li>
+        <li className={css.profileStatsEl}>
+          <span>Views</span>
+          <span className={css.profileStatsNumber}>{stats.views}</span>
+        </li>
+        <li className={css.profileStatsEl}>
+          <span>Likes</span>{" "}
+          <span className={css.profileStatsNumber}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
